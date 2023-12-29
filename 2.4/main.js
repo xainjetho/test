d823aecbee1f387c00582ccdeb3e589dae8da57f
xainjetho <muffler7@mail.ru>
2023-12-29 10:27:46 +0700

    lesson03

---
+++ 2.4/main.js
@@ -0,0 +1,13 @@
+let titleProject = 'Оценка стоимости';
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+let screenPrice = 5000;
+let percentage = 5;
+let fullPrice = 100000;
+let responsive = true;
+
+console.log(titleProject);
+console.log(screensValue);
+console.log(screenPrice);
+console.log(percentage);
+console.log(fullPrice);
+console.log(responsive);
